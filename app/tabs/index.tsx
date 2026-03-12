c843572528e31a83fad14074cb85c8ee677f1472
kemsguy7 <mattidungafa@gmail.com>
2026-03-12 16:36:06 +0100

    feat: created imageViewer component and styles

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+
+const placeHolderImage = require('@/assets/images/background-image.png');
+
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeHolderImage} />
+      </View>
       <Text style={styles.text}>My first react native app.</Text>
       <Link href='/tabs/about' style={styles.button}>
         Go to About screen
@@ -18,6 +24,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
   text: { color: '#fff', fontSize: 20, fontWeight: 'bold' },
   button: {
